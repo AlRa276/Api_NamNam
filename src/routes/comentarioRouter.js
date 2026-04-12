f16237e462bf92252cf404c3ea7b13de6da0eead
AlRa276 <243760@ids.upchiapas,edu.mx>
2026-04-12 06:25:31 -0600

    Nueva ruta listar todos los comentarios

--- src/routes/comentarioRouter.js
+++ src/routes/comentarioRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const  comentarios  = require('../controllers/comentarioController');
 
-const { verificarToken } = require('../middlewares/auth');
+const { verificarToken, soloAdmin } = require('../middlewares/auth');
 
 router.get('/receta/:id_receta',  comentarios.listar);
 router.post('/receta/:id_receta', verificarToken, comentarios.crear);
